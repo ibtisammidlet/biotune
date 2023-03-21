b09d37b83e04af1a1e31df41b90a06fddf3b24de
ibtisam midlet <storebitid@gmail.com>
2023-03-21 17:00:13 +0100

    *

---
+++ frontend/XOMISSE_Custom_EU_Cookies_Notice.js
@@ -0,0 +1,5 @@
+  cookieOptions = { 
+    msg: "welcome to the dark side, we have cookies +_+", 
+    link: "https://www.blogger.com/go/blogspot-cookies", 
+    close: "Okay!", 
+    learn: "let me know more" }; 
